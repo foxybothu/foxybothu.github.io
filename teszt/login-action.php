757cedc87f4b45bbc3cc5563d7819fd7ff390ce7
foxybothu <84864927+foxybothu@users.noreply.github.com>
2021-10-02 20:10:56 +0200

    Update login-action.php

--- teszt/login-action.php
+++ teszt/login-action.php
@@ -34,3 +34,52 @@ final class OpenApiFactory implements OpenApiFactoryInterface
         return $openApi->withPaths($filteredPaths);
     }
 }
+<?php
+namespace Phppot;
+
+use \Phppot\Member;
+if (! empty($_POST["login"])) {
+    session_start();
+    $username = filter_var($_POST["user_name"], FILTER_SANITIZE_STRING);
+    $password = filter_var($_POST["password"], FILTER_SANITIZE_STRING);
+    require_once (__DIR__ . "./class/Member.php");
+    
+    $member = new Member();
+    $isLoggedIn = $member->processLogin($username, $password);
+    if (! $isLoggedIn) {
+        $_SESSION["errorMessage"] = "Invalid Credentials";
+    }
+    header("Location: ./index.php");
+    exit();
+}
+<?php
+namespace Phppot;
+
+use \Phppot\Member;
+
+if (! empty($_SESSION["userId"])) {
+    require_once __DIR__ . './../class/Member.php';
+    $member = new Member();
+    $memberResult = $member->getMemberById($_SESSION["userId"]);
+    if(!empty($memberResult[0]["display_name"])) {
+        $displayName = ucwords($memberResult[0]["display_name"]);
+    } else {
+        $displayName = $memberResult[0]["user_name"];
+    }
+}
+?>
+<html>
+<head>
+<title>User Login</title>
+<link href="./view/css/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <div>
+        <div class="dashboard">
+            <div class="member-dashboard">Welcome <b><?php echo $displayName; ?></b>, You have successfully logged in!<br>
+                Click to <a href="./logout.php" class="logout-button">Logout</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
